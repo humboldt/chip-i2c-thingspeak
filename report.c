8a8d5bd4e2cd0ff03b9938c60356a376b6827409
Vencislav Atanasov <user890104@users.noreply.github.com>
2016-12-05 01:00:59 +0200

    Reuse the URL buffer, include only configured drivers

--- report.c
+++ report.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "axp209.h"
-#include "si7021.h"
-
 #include "config.h"
 
-int main(int argc, char **argv) {
-	char buf_axp209[2048];
-	strcpy(buf_axp209, "http://api.thingspeak.com/update?api_key=" API_KEY_AXP209);
-	axp209_write_data(buf_axp209, I2C_PORT_AXP209);
-	puts(buf_axp209);
+#if defined(API_KEY_AXP209) && defined(I2C_PORT_AXP209)
+#include "axp209.h"
+#endif
 
-	char buf_si7021[2048];
-	strcpy(buf_si7021, "http://api.thingspeak.com/update?api_key=" API_KEY_SI7021);
-	si7021_write_data(buf_si7021, I2C_PORT_SI7021);
-	puts(buf_si7021);
+#if defined(API_KEY_SI7021) && defined(I2C_PORT_SI7021)
+#include "si7021.h"
+#endif
+
+int main(int argc, char **argv) {
+	char buffer[2048];
+
+#if defined(API_KEY_AXP209) && defined(I2C_PORT_AXP209)
+	strcpy(buffer, "http://api.thingspeak.com/update?api_key=" API_KEY_AXP209);
+	axp209_write_data(buffer, I2C_PORT_AXP209);
+	puts(buffer);
+#endif
+
+#if defined(API_KEY_SI7021) && defined(I2C_PORT_SI7021)
+	strcpy(buffer, "http://api.thingspeak.com/update?api_key=" API_KEY_SI7021);
+	si7021_write_data(buffer, I2C_PORT_SI7021);
+	puts(buffer);
+#endif
 
 	return 0;
 }
